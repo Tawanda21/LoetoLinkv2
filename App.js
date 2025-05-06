b4c83a4e6b1fca440b1a60ee0999cf00386ef56a
Kelly Modise <cse23-110@thuto.bac.co.bw>
2025-05-07 01:48:59 +0200

    | removed the login thing

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { StatusBar, SafeAreaView } from 'react-native';
 import AppNavigator from './navigation/AppNavigator';
-
+//
 
 export default function App() {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
+      <StatusBar barStyle="dark-content" /> 
       <SafeAreaView>
       </SafeAreaView>
       <AppNavigator />
